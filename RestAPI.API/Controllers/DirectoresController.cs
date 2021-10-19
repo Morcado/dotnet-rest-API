e7e317d831f2891265ddc6e5e42488d87ce52063
Oscar Armando Gonzalez Patiño <oagonzalez@elpotosi.com.mx>
2021-10-19 10:01:43 -0500

    Create DirectoresController.cs

---
+++ RestAPI.API/Controllers/DirectoresController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestAPI.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAPI.API.Controllers
+{
+    [ApiController]
+    [Route("directores")]
+    public class DirectoresController : ControllerBase
+    {
+        // GET: obtener directores
+        [HttpGet]
+        public ActionResult<IEnumerable<Director>> ObtenerDirectores()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{idDirector}")]
+        // GET: obtener director
+        public ActionResult<Director> ObtenerDirector(Guid idDirector)
+        {
+            return Ok();
+        }
+
+        // POST: crear director
+        [HttpPost]
+        public ActionResult CrearDirector()
+        {
+            return Ok();
+        }
+    }
+}
